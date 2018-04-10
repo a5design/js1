70148c64f271be9f74bdc99b341366dd6656a794
a5design <a5design@comcast.net>
2018-04-09 21:05:04 -0700

    Script stuff

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var myHistory = "I am a designer. I want to learn to code because I want full control. I have been working at my current job for many years and we contract out a lot of web deign (including jobs to Bitwise). I plan on learning asa much as I can so that we can keep more in house.";
+console.log(myHistory);
+window.confirm(myHistory);
